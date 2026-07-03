eeab3063f4f181e6988e7668a6a02808510552b9
bbtbeh-lang <bbtbeh@gmail.com>
2026-07-03 11:34:22 -0400

    Delete finsnap-stripe-integration directory

--- finsnap-stripe-integration/lib/stripe.ts
+++
@@ -1,7 +0,0 @@
-import Stripe from 'stripe';
-
-// This file must only ever be imported from server-side code (API routes).
-// STRIPE_SECRET_KEY is never exposed to the browser.
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2026-06-24.dahlia',
-});
